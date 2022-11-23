0df8e12a084361ac0f58211d3c29d6a4d6dcafda
Gaurav Kale <gauravkale100@gmail.com>
2022-11-22 16:06:54 -0800

    Tweaked pong commands

--- M5/central.js
+++ M5/central.js
@@ -4,15 +4,24 @@
 
 const noble = require('@abandonware/noble');
 
-const uuid_service = "1101"
+const uuid_service = "1102"
+const uuid_service_gyro = "1105"
+
 const x_uuid = "2101"
 const y_uuid = "2102"
 const z_uuid = "2103"
+
+const gx_uuid = "3101"
+const gy_uuid = "3102"
+const gz_uuid = "3103"
 
 let  sensorValue = NaN
 let  sensorValueX = NaN
 let  sensorValueY = NaN
 let  sensorValueZ = NaN
+let  sensorValueGX = NaN
+let  sensorValueGY = NaN
+let  sensorValueGZ = NaN
 
 noble.on('stateChange', async (state) => {
     if (state === 'poweredOn') {
@@ -21,15 +30,26 @@ noble.on('stateChange', async (state) => {
     }
 });
 
+noble.on('stateChange', async (state) => {
+    if (state === 'poweredOn') {
+        console.log("start scanning")
+        await noble.startScanningAsync([uuid_service_gyro], false);
+    }
+});
+
 noble.on('discover', async (peripheral) => {
     await noble.stopScanningAsync();
     await peripheral.connectAsync();
     const {
         characteristics
-    } = await peripheral.discoverSomeServicesAndCharacteristicsAsync([uuid_service],[x_uuid,y_uuid,z_uuid]);
+    } = await peripheral.discoverSomeServicesAndCharacteristicsAsync([uuid_service,uuid_service_gyro],[x_uuid,y_uuid,z_uuid,gx_uuid,gy_uuid,gz_uuid]);
     readData(characteristics[0])
     readData(characteristics[1])
     readData(characteristics[2])
+    readData(characteristics[3])
+    readData(characteristics[4])
+    readData(characteristics[5])
+
 });
 
 //
@@ -38,7 +58,7 @@ noble.on('discover', async (peripheral) => {
 let readData = async (characteristic) => {
     const value = (await characteristic.readAsync());
     sensorValue = value.readFloatLE(0);
-    console.log(characteristic.uuid + ": " + sensorValue);
+    // console.log(characteristic.uuid + ": " + sensorValue);
     if (characteristic.uuid == "2101") {
         sensorValueX = value.readFloatLE(0);
     }
@@ -48,11 +68,21 @@ let readData = async (characteristic) => {
     if (characteristic.uuid == "2103") {
         sensorValueZ = value.readFloatLE(0);
     }
+    if (characteristic.uuid == "3101") {
+        sensorValueGX = value.readFloatLE(0);
+    }
+    if (characteristic.uuid == "3102") {
+        sensorValueGY = value.readFloatLE(0);
+    }
+    if (characteristic.uuid == "3103") {
+        console.log(characteristic.uuid + ": " + sensorValue);
+        sensorValueGZ = value.readFloatLE(0);
+    }
 
     // read data again in t milliseconds
     setTimeout(() => {
         readData(characteristic)
-    }, 10);
+    }, 5);
 }
 
 
@@ -73,7 +103,10 @@ app.post('/', (req, res) => {
     res.end(JSON.stringify({
         aX: sensorValueX,
         aY: sensorValueY,
-        aZ: sensorValueZ
+        aZ: sensorValueZ,
+        gX: sensorValueGX,
+        gY: sensorValueGY,
+        gZ: sensorValueGZ
     }))
 })
 
